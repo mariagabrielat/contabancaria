dc971ceb4648662c66731963de79626ca2856eb8
mariagabrielat <mariagabrielatech@gmail.com>
2025-02-12 18:54:18 -0300

    Criação da Conta

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -2,12 +2,21 @@ package conta;
 
 import java.util.Scanner;
 
+import conta.model.Conta;
 import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Conta c1 = new Conta(1, 123, 1, "Maria", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
+		
 		Scanner leia = new Scanner(System.in);
 
 		int opcao;
@@ -87,8 +96,8 @@ public class Menu {
 	public static void sobre() {
 		System.out.println("\n*********************************************************");
 		System.out.println("Projeto Desenvolvido por: ");
-		System.out.println("Generation Brasil - generation@generation.org");
-		System.out.println("github.com/conteudoGeneration");
+		System.out.println("Maria S. - marias@genstudents.org");
+		System.out.println("https://github.com/mariagabrielat");
 		System.out.println("*********************************************************");
 
 	}
